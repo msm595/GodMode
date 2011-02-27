1ec501f2fdcd523801806671028c2f241dba5c22
Alex <Alex@Alex-VAIO>
2011-02-26 21:12:45 -0500

    can detect permissions

--- src/com/msm595/GodMode/GodMode.java
+++ src/com/msm595/GodMode/GodMode.java
@@ -6,10 +6,16 @@ import java.util.HashMap;
 import org.bukkit.Server;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
+import org.bukkit.event.server.ServerListener;
+import org.bukkit.event.server.PluginEvent;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginLoader;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.PluginManager;
+import com.nijiko.permissions.PermissionHandler;
+import com.nijikokun.bukkit.Permissions.Permissions;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
 * Sample plugin for Bukkit
@@ -17,14 +23,35 @@ import org.bukkit.plugin.PluginManager;
 * @author Dinnerbone
 */
 public class GodMode extends JavaPlugin {
+    public static final Logger log = Logger.getLogger("Minecraft");
     private final GMPlayerListener playerListener = new GMPlayerListener(this);
     private final GMEntityListener entityListener = new GMEntityListener(this);
+    
     private boolean godOnLogin = true;
+    
+    private boolean usePermissions = false;
+    public static PermissionHandler Permissions;
+        private Listener Listener = new Listener();
+
+    private class Listener extends ServerListener {
+
+        public Listener() {
+        }
+
+        @Override
+        public void onPluginEnabled(PluginEvent event) {
+            if(event.getPlugin().getDescription().getName().equals("Permissions")) {
+                GodMode.Permissions = ((Permissions)event.getPlugin()).Security;
+                System.out.println("[GodMode] Attached plugin to Permissions. Enjoy~");
+                usePermissions=true;
+            }
+        }
+    }
 
     //Create the hashmap for gods. integer values are a binary permission system:
     //1:invincible, 2:put out fire instantly (if they are caught on fire), 4:nothing so far
     //each separate permission is a power of 2 eg: 2^0=1, 2^1=2, etc...
-    //permissions can be combined simply by adding (1+2 would be invincible and fires would be put out)
+    //permissions can be combined simply by adding (1+2=3 would be invincible and fires would be put out)
     public final HashMap<Player, Integer> gods = new HashMap();  
 
 
@@ -41,11 +68,9 @@ public class GodMode extends JavaPlugin {
     public void onEnable() {
         //Create the pluginmanage pm.
         PluginManager pm = getServer().getPluginManager();
-        //Create PlayerCommand listener
         pm.registerEvent(Event.Type.PLAYER_COMMAND, playerListener, Event.Priority.Normal, this);
-        //Create BlockPlaced listener
         pm.registerEvent(Event.Type.ENTITY_DAMAGED, entityListener, Event.Priority.Normal, this);
-        //Get the infomation from the yml file.
+        pm.registerEvent(Event.Type.PLUGIN_ENABLE, Listener, Event.Priority.Monitor, this);
         PluginDescriptionFile pdfFile = this.getDescription();
         //Print that the plugin has been enabled!
         System.out.println( "["+pdfFile.getName().toUpperCase() + "] version " + pdfFile.getVersion() + " is enabled!" );
